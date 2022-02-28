510d42c9a9cc83731135daf31e7c8c151a95b034
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-02-28 13:51:44 -0300

    some changes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Gowenland's console");
 };
